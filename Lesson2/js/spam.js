7f2a7b374ba6a99b51b02c72500032a2f81dd7cf
Bohdan <bogmira777@gmail.com>
2023-10-18 20:34:21 +0200

    Add spam task

--- Lesson2/js/spam.js
+++ Lesson2/js/spam.js
@@ -1,10 +1,35 @@
 "use strict";
 
-const sentence = "Lorem, Spam, hej spaM";
+// const sentence = "Lorem, Spam, hej spaM";
 
-function checkString(str, spamTrigger = "spam") {
-  let newStr = str.trim().toLowerCase();
-  return newStr.replaceAll(spamTrigger, "***");
+// function checkString(str, spamTrigger = "spam") {
+//   let newStr = str.trim().toLowerCase();
+//   return newStr.replaceAll(spamTrigger, "***");
+// }
+
+// console.log(checkString(sentence));
+
+function replaceSpam(inputString) {
+  const lowerCaseInput = inputString.toLowerCase();
+  if (lowerCaseInput.includes("spam")) {
+    return inputString.replace(/spam/gi, "***");
+  }
+  return inputString;
 }
 
-console.log(checkString(sentence));
+function replaceSpamInArray(stringArray) {
+  const modifiedArray = stringArray.map((str) => {
+    return replaceSpam(str);
+  });
+  return modifiedArray;
+}
+
+const text = "Це текст SPAM із словом Spam!";
+console.log(replaceSpam(text));
+
+const textArray = [
+  "Це текст із словом Spam та ще spAm!",
+  "Ще один рядок без spam.",
+];
+const modifiedArray = replaceSpamInArray(textArray);
+console.log(modifiedArray);
