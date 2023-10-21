ace3df73df09bd9b13e95dd577bc6ad207bd1b2e
Bohdan Scherbak <bogmira777@gmail.com>
2023-10-21 09:47:32 +0200

    Add palindrom task

--- Lesson2/js/palindrom.js
+++ Lesson2/js/palindrom.js
@@ -1,2 +1,22 @@
 "use strict";
 
+let arr = [
+  "А роза упала на лапу Азора",
+  "Ого",
+  "А Собака боса",
+  "Привет, мир",
+  "А Собака боса",
+  "Лёша на полке клопа нашёл",
+  "Я — арка края",
+];
+
+function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[^a-zа-яё]/g, "");
+  return str === str.split("").reverse().join("");
+}
+
+function checkPalindroms(arr) {
+  return arr.filter(isPalindrome);
+}
+
+console.log(checkPalindroms(arr));
